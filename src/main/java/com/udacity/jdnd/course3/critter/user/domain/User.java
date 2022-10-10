51671a337617d00eb1951e3e35f61249bf1efc21
kevin <onofrekevin@gmail.com>
2022-10-09 21:15:14 -0500

    Se agrego anotaciones a clase user.

--- src/main/java/com/udacity/jdnd/course3/critter/user/domain/User.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.udacity.jdnd.course3.critter.user.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.*;
@@ -11,11 +12,35 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
+    @JsonProperty
     private Long id;
 
     @Nationalized
-    @Column(name="userName", length = 8)
+    @Column(name="userName", length = 8, nullable = true, unique = true)
+    @JsonProperty
     private String userName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(nullable = false)
+    @Nationalized
+    private String password;
+
+    @Nationalized
+    @JsonProperty
+    private String firstName;
+    @Nationalized
+    @JsonProperty
+    private String lastName;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
 
     public String getUserName() {
         return userName;
@@ -47,21 +72,5 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    @Nationalized
-    private String password;
-    @Nationalized
-    private String firstName;
-    @Nationalized
-    private String lastName;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    public Long getId() {
-        return id;
     }
 }
