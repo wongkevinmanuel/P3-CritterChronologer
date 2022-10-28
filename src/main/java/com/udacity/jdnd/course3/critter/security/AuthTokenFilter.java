0066bc5d27efb47c18d9ea852b5cffece76f8515
kevin <onofrekevin@gmail.com>
2022-10-27 20:56:03 -0500

    Se refactorizo metodo doFilterInternal. Para definir filtro por solicitud.

--- src/main/java/com/udacity/jdnd/course3/critter/security/AuthTokenFilter.java
+++ src/main/java/com/udacity/jdnd/course3/critter/security/AuthTokenFilter.java
@@ -27,11 +27,33 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
             throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails,
+                                null,
+                                userDetails.getAuthorities());
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+
+        filterChain.doFilter(request, response);
 
     }
 
     private String parseJwt(HttpServletRequest request){
         String jwt = jwtUtils.getJwtFromCookies(request);
+
         return jwt;
     }
 }
