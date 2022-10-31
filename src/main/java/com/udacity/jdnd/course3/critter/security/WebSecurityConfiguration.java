5effdfb6e5ff7e750a2dccbcd0e736d41af9db84
kevin <onofrekevin@gmail.com>
2022-10-30 23:15:22 -0500

    Refactorizo clase de configuración de seguridad web de Spring Security sin WebSecurityConfigurerAdapter.

--- src/main/java/com/udacity/jdnd/course3/critter/security/WebSecurityConfiguration.java
+++ src/main/java/com/udacity/jdnd/course3/critter/security/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -49,6 +50,12 @@ public class WebSecurityConfiguration { //extends WebSecurityConfigurerAdapter {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+    throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
     /*
     //Define los recursos públicos. A continuación, hemos establecido el
