503e1dd74289a51900a2e7d5f636930caa1d7857
kevin <onofrekevin@gmail.com>
2022-10-31 23:10:11 -0500

    Refactorizo clase de configuración de seguridad web de Spring Security, agrego Bean a clase.

--- src/main/java/com/udacity/jdnd/course3/critter/security/WebSecurityConfiguration.java
+++ src/main/java/com/udacity/jdnd/course3/critter/security/WebSecurityConfiguration.java
@@ -1,23 +1,33 @@
 package com.udacity.jdnd.course3.critter.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 //@EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true
+)
 public class WebSecurityConfiguration { //extends WebSecurityConfigurerAdapter {
     private UserDetailsServiceImpl userDetailsService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private AuthEntryPointJwt unauthorizedHandler;
+
+    @Bean
+    public AuthTokenFilter authTokenFilter(){
+        return new AuthTokenFilter();
+    }
 
     public WebSecurityConfiguration(BCryptPasswordEncoder b, UserDetailsServiceImpl u
     ){
