6fe1f38f08e0112a6913991167f08ee3d3c041ec
kevin <onofrekevin@gmail.com>
2022-11-16 20:33:16 -0500

    Se refactorizo metodos para  login.

--- src/main/java/com/udacity/jdnd/course3/critter/login/requests/CreateUserRequest.java
+++ src/main/java/com/udacity/jdnd/course3/critter/login/requests/CreateUserRequest.java
@@ -1,4 +1,40 @@
 package com.udacity.jdnd.course3.critter.login.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CreateUserRequest {
+    @JsonProperty
+    private String userName;
+
+    @JsonProperty
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @JsonProperty
+    private String confirmPassword;
+
+
 }
