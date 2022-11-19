4d9b3e9076ac7609f4176da09dd1b29d3eb0f367
kevin <onofrekevin@gmail.com>
2022-11-18 19:24:25 -0500

    Se agrego clase que ayuda a inyectar objetos en  test login.

--- src/test/java/com/udacity/jdnd/course3/critter/TestUtils.java
+++ src/test/java/com/udacity/jdnd/course3/critter/TestUtils.java
@@ -1,4 +1,29 @@
 package com.udacity.jdnd.course3.critter;
 
+import java.lang.reflect.Field;
+
+//Ayuda a inyectar objetos
 public class TestUtils {
+    public static void InyectarObjeto(Object blancoObjetivo, String nombreCampo
+            , Object aInyectar) {
+        boolean privado = false;
+        Field field = null;
+
+        try {
+            field = blancoObjetivo.getClass().getDeclaredField(nombreCampo);
+            if (!field.isAccessible()) {
+                field.setAccessible(true);
+                privado = true;
+            }
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            //Establece un objeto con el objeto q quiero inyectar
+            field.set(blancoObjetivo, aInyectar);
+            if(privado)
+                field.setAccessible(false);
+
+        } catch (IllegalAccessException e) {        }
+    }
 }
