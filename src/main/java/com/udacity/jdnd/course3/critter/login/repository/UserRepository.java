9117f9781eba55d0498d7d5a79dc6513f6e31151
kevin <onofrekevin@gmail.com>
2023-02-02 20:50:37 -0500

    Refactorizo UserRepository.

--- src/main/java/com/udacity/jdnd/course3/critter/login/repository/UserRepository.java
+++ src/main/java/com/udacity/jdnd/course3/critter/login/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.udacity.jdnd.course3.critter.login.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
     User findByUserName(String username);
-
+    Optional<User> findByFirstName(String nameFirst);
 }
