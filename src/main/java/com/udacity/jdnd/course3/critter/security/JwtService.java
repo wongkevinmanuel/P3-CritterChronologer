40ce58c68f2d80f747ff0c54df8427dbeca08408
kevin <onofrekevin@gmail.com>
2023-02-05 22:58:30 -0500

    Se implemento metodo extracAllClaims.

--- src/main/java/com/udacity/jdnd/course3/critter/security/JwtService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/security/JwtService.java
@@ -1,10 +1,30 @@
 package com.udacity.jdnd.course3.critter.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
     public String extraerUserName(String token) {
         return "";
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInkey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInkey(){
+        byte[] keyBytes = Decoders.BASE64.decode("${env.SECRET_KEY}");
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
