cf1f2668a4f0654207866ad1b2f73956bf4cee9a
kevin <onofrekevin@gmail.com>
2023-02-06 22:47:59 -0500

    Se implemento metodos para gestionar el token.

--- src/main/java/com/udacity/jdnd/course3/critter/security/JwtService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/security/JwtService.java
@@ -2,17 +2,24 @@ package com.udacity.jdnd.course3.critter.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     public String extraerUserName(String token) {
-        return "";
+        String claimsUser = extractClaim(token, Claims::getSubject);
+        return claimsUser;
     }
 
     private Claims extractAllClaims(String token){
@@ -23,6 +30,24 @@ public class JwtService {
                 .getBody();
     }
 
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis())) //24 horas y 1000 milesegundos
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSignInkey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(),userDetails);
+    }
     private Key getSignInkey(){
         byte[] keyBytes = Decoders.BASE64.decode("${env.SECRET_KEY}");
         return Keys.hmacShaKeyFor(keyBytes);
