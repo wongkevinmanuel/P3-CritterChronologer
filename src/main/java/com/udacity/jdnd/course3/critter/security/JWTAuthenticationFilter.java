e4f6839fa2d5f0593df975ffbf69b2aeb2525984
kevin <onofrekevin@gmail.com>
2023-03-08 23:38:16 -0500

    Se implemento WebSecurityConfigurerAdapter para asegurar API con autenticación básica.

--- src/main/java/com/udacity/jdnd/course3/critter/security/JWTAuthenticationFilter.java
+++ src/main/java/com/udacity/jdnd/course3/critter/security/JWTAuthenticationFilter.java
@@ -1,12 +1,7 @@
 package com.udacity.jdnd.course3.critter.security;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
--- src/main/java/com/udacity/jdnd/course3/critter/security/WebSecurityConfiguration.java
+++ src/main/java/com/udacity/jdnd/course3/critter/security/WebSecurityConfiguration.java
@@ -3,75 +3,38 @@ package com.udacity.jdnd.course3.critter.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-//Clase de configuracion
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration {
-    private UserDetailsServiceImpl userDetailsService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
 
-    @Bean
-    public AuthTokenFilter authTokenFilter(){
-        return new AuthTokenFilter();
-    }
-
-    public WebSecurityConfiguration(BCryptPasswordEncoder b, UserDetailsServiceImpl u
-    ){
-       this.userDetailsService  = u;
-       this.bCryptPasswordEncoder = b;
-    }
-
-    //Define los recursos públicos. A continuación, hemos establecido el
-    // punto final SIGN_UP_URL como público. El http.cors() se utiliza para
-    // hacer que Spring Security admita CORS (Cross-Origin Resource Sharing)
-    // y CSRF (Cross-Site Request Forgery).
-    @Bean
-    public SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception {
-        JWTAuthenticationFilter jwtAuthFilter;
-        AuthenticationProvider authenticationProvider;
-        http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                //.antMatchers(HttpMethod.POST, JWTPersonalSecurityConstants.SIGN_UP_URL).permitAll()
-                .requestMatchers("")
-                .permitAll()
+        http.csrf().disable()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .sessionManagement()
-                //Politica de creacion de session (Sin Estado)
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .httpBasic();
     }
 
-    @Override
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder().encode("password"))
+                .roles("USER");
+    }
+
     @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-
-    //Declara BCryptPasswordEncoder como la técnica de codificación
-    //y carga datos específicos del usuario.
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.parentAuthenticationManager(authenticationManagerBean())
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(bCryptPasswordEncoder);
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
